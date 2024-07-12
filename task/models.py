da70246b6d7eb26148c1d7534a376568edfed2b7
F4rab1 <issafarabi2022@gmail.com>
2024-07-12 23:35:50 +0500

    task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    IN_PROGRESS = 'in_progress'
+    COMPLETED = 'completed'
+
+    STAGE_CHOICES = [
+        (IN_PROGRESS, 'In Progress'),
+        (COMPLETED, 'Completed'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    stage = models.CharField(max_length=20, choices=STAGE_CHOICES, default=IN_PROGRESS)
+    start_time = models.DateTimeField()
+    due_to = models.DateTimeField()

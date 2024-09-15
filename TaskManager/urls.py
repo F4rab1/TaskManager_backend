538a05569f2287d545fa921be50d1131decdd4dc
F4rab1 <issafarabi2022@gmail.com>
2024-09-15 23:47:49 +0500

    url for registration

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -17,13 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from djoser.views import UserViewSet
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register(r'register', UserViewSet, basename='user')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('task.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('api/', include(router.urls)),  # Custom API registration route
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('djoser.urls')), 
     path('auth/', include('djoser.urls.jwt')), 
 ]
+
